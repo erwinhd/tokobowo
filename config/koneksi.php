411bcc994bc8ca82e36763fec881d214c00f6776
ERWIN CATUR PRASETYO <51075666+erwinhd@users.noreply.github.com>
2023-03-06 12:49:07 +0700

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+	$host = "localhost"; 
+	$username = "id20278017_erwin";
+	$password = "4Yqwj^[gK<HJ*BiG"; 
+	$database = "id20278017_tokobowo";
+	mysql_connect($host,$username,$password) or die ("Harap Periksa Koneksi Database Anda!");
+	mysql_select_db($database) or die ("Database Tidak Ditemukan!");
+?>
